6a43193fa6cacc9dd18d1f9ad3387feb2656ea24
Midzi Ribery <muhd.termidzi@gmail.com>
2022-10-04 21:37:05 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1> Hello World</h1>
+       <button>Can press</button>
+       <p>but after pressing the button, how to link or add listener?</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
